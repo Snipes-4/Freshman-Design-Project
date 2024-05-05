9286e9d56889769c6c40dae4631d16fd286b0563
Kylie <kgk009@latech.edu>
2024-05-05 15:04:03 -0500

    Modified GUI and split the KV Language into a new file.

--- GUI/main.py
+++ GUI/main.py
@@ -7,152 +7,9 @@ from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.lang import Builder
 from kivy.uix.scrollview import ScrollView
 from kivy.graphics import Rectangle, Color
+from decoder import Decoder
 
-Builder.load_string("""
-#:set DECODE_COLOR "#7D2F5C"
-#:set ENCODE_COLOR "#EA3033"
-#:set LEARN_COLOR "#4A6C6F" 
-#:set BACKGROUND_COLOR "#32322C"
-
-<MainScreenButton@Button>
-    text_size: self.size
-    font_size: 60
-    size_hint:(.75, 1)
-    pos_hint: {'center_x': .5, 'center_y': .5}
-    valign: "middle"
-    halign: "center"
-    bold: "True"
-    background_normal: ""
-    
-<MainScreen>
-    BoxLayout:
-        orientation: "vertical"
-        padding: 50
-        spacing: 30
-        MainScreenButton:
-            text: "Decode"
-            background_color: DECODE_COLOR
-            on_release:
-                root.manager.transition.direction = "up"
-                root.manager.current = "decode"
-                
-        MainScreenButton:
-            text: "Encode"
-            background_color: ENCODE_COLOR
-            on_release:
-                root.manager.transition.direction = "up"
-                root.manager.current = "encode"
-        MainScreenButton:
-            text: "Learn"
-            background_color: LEARN_COLOR
-            on_release:
-                root.manager.transition.direction = "up"
-                root.manager.current = "learn"
-            
-<BackArrow@Button>
-    pos_hint: {"top":1, "left":1, "x":.005}
-    size_hint: None, None
-    size: 60,60
-    background_color: (0,0,0,0)
-    Image:
-        source: "images/backarrow.png"
-        pos: self.parent.pos
-        size_hint: None, None
-        size: self.parent.size
-        
-<EncodeScreen>
-    FloatLayout:
-        BackArrow:
-            on_release: root.manager.current = "main"
-        TextInput:
-            id: userinput
-            focus: True
-            hint_text: "Input message"
-            size_hint: .4, .2
-            size: self.parent.x, self.parent.y
-            pos_hint: {"bottom":1, "left":1}
-            padding_y: [self.height / 2.0 - (self.line_height / 2.0) * len(self._lines), 0]
-            font_size: 25
-            halign: "center"
-            multiline:False
-            on_text_validate: encodedmessage.text="encoded ory ong and sawdawdawdwadawdawdawdawdwag" # this is how our message gets encoded
-        Label:
-            text: "User Input"
-            size_hint: userinput.size_hint
-            font_size: userinput.font_size
-            pos: userinput.x, userinput.y+80
-        TextInput:
-            id: keyinput
-            hint_text: "Input cipher key"
-            size_hint: .4, .2
-            size: self.parent.x, self.parent.y
-            pos_hint: {"bottom":1, "right":1}
-            padding_y: [self.height / 2.0 - (self.line_height / 2.0) * len(self._lines), 0]
-            font_size: 25
-            halign: "center"
-            focus: False
-        Label:
-            text: "Encoding Key"
-            size_hint: userinput.size_hint
-            font_size: userinput.font_size
-            pos: keyinput.x, keyinput.y+80
-        Label:
-            id: encodedmessage
-            size_hint_y: None
-            text_size: self.width, None
-            font_size: 40
-            height: self.texture_size[1]
-            pos_hint: {"center_y":.6}
-            padding_x: 30
-            
-            
-            
-<DecodeScreen>
-    FloatLayout:
-        BackArrow:
-            on_release: root.manager.current = "main"
-        GridLayout:
-            rows:3
-            rows_minimum: {0: 20, 1: 20, 2: 50}
-            Label:
-                text:"uhh eerm"
-                size_hint_y: .3
-            Label:
-                text:"what the spruce"
-                size_hint_y: .3
-                
-            GridLayout: 
-                cols:3
-                GridLayout:
-                    rows:2
-                    TextInput:
-                        hint_text: "Selected Letter"
-                    TextInput:
-                        hint_text: "Replace with"
-                GridLayout:
-                    rows:3
-                    Button:
-                        text:"Clear"
-                    Button:
-                        text:"Undo"
-                    Button:
-                        text:"Replace"
-                Label:
-                    font_size: '40sp'
-                    outline_color: 0, 0, 0
-                    outline_width: 1
-                    text: 'frequency'
-                    
-    
-            
-            
-            
-<LearnScreen>
-    FloatLayout:                
-        BackArrow:  
-            on_release: root.manager.current = "main"  
-""")
-
+Builder.load_file("main.kv")
 
 class MainScreen(Screen):
     pass
@@ -169,6 +26,15 @@ class DecodeScreen(Screen):
 class LearnScreen(Screen):
     pass
 
+class InputScreen(Screen):
+    pass
+
+class WithKeyScreen(Screen):
+    pass
+
+class WithoutKeyScreen(Screen):
+    pass
+
 
 class Test(App):
     def build(self):
@@ -179,6 +45,9 @@ class Test(App):
         sm.add_widget(EncodeScreen(name='encode'))
         sm.add_widget(DecodeScreen(name='decode'))
         sm.add_widget(LearnScreen(name='learn'))
+        sm.add_widget(InputScreen(name='input'))
+        sm.add_widget(InputScreen(name='withoutkey'))
+        sm.add_widget(InputScreen(name='withkey'))
         return sm
 
 
